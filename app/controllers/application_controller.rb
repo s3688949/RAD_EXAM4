05555789ed61203b2204b32f6e3955f8f4dbbf85
s3688949@student.rmit.edu.au <s3688949@student.rmit.edu.au>
2021-06-17 17:39:36 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,6 @@ class ApplicationController < ActionController::Base
     if not cookies[:userId]
       cookies[:userId] = Time.now.to_f
       h1= History.create(userId: cookies[:userId])
-      cookies[:completed] = 0
-      cookies[:correct] = 0
-      cookies[:max] = 4
-      list = Array.new(60) { |i| 1 * i + 1}
-      list = list.shuffle()
-      cookies[:list] = (list.class == Array) ? list.join(',') : ''
     end
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,11 @@ class HomeController < ApplicationController
   before_action :session?
   
   def index
-    puts cookies[:list]
+    cookies[:completed] = 0
+    cookies[:correct] = 0
+    cookies[:max] = 4
+    list = Array.new(60) { |i| 1 * i + 1}
+    list = list.shuffle()
+    cookies[:list] = (list.class == Array) ? list.join(',') : ''
   end
 end
