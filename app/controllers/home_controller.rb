501b1c8cd9e9aa95fe26d7649f45b91589be0c1a
s3688949@student.rmit.edu.au <s3688949@student.rmit.edu.au>
2021-06-17 08:26:53 +0000

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,4 @@ class HomeController < ApplicationController
   before_action :session?
   def index
   end
-  
-  def kill
-    if cookies[:userId]
-      cookies[:userId] = nil
-      cookies[:compelted] = nil
-      cookies[:correct] = nil
-      cookies[:max] = nil
-      cookies[:list] = nil
-    end
-    redirect_to "/"
-  end
 end
