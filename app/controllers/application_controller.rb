936f343d8767c594b55fef246353ad633746d2e3
s3688949@student.rmit.edu.au <s3688949@student.rmit.edu.au>
2021-06-17 07:29:22 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+    cookies[:storage] = "HOPEFULLY THIS CARRIES"
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    puts cookies[:storage]
     cookies[:storage] = "HOPEFULLY THIS CARRIES"
     puts cookies[:storage]
   end
