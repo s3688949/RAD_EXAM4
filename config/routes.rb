a1da1bb608e96db2ef39aa60cd853006208e9eaa
s3688949@student.rmit.edu.au <s3688949@student.rmit.edu.au>
2021-06-17 06:08:55 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'questions/index', to: 'questions#index',
+  
+  resources :histories
+  resources :attempts
+  resources :questions
+  resources :answers
 
   root :to => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
