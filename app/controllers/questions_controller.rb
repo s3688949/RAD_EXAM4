1d44ca06c88ea41182b4d7cf47156e408a20ba20
s3688949@student.rmit.edu.au <s3688949@student.rmit.edu.au>
2021-06-17 17:02:06 +0000

    update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -28,7 +28,7 @@ class QuestionsController < ApplicationController
   def completed
      @h1 = History.all.where(userId: cookies[:userId].to_f)
      h2 = History.all.where(userId: cookies[:userId].to_f)
-     Attempts.create(history_id: h2.id, time: Time.now, correct: cookies[:correct].to_i, questions: cookies[:max], difficulty: "All", category: "All")
+     Attempt.create(history_id: h2.id, time: Time.now, correct: cookies[:correct].to_i, questions: cookies[:max], difficulty: "All", category: "All")
   end
 
   # POST /questions or /questions.json
