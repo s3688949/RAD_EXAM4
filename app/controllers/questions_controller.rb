3903ddc8bcfff227ff8114c2ea822d169e02a7c5
s3688949@student.rmit.edu.au <s3688949@student.rmit.edu.au>
2021-06-17 13:47:46 +0000

    update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: %i[ show edit update destroy ]
+  skip_before_action :verify_authenticity_token
 
   # GET /questions or /questions.json
   def index
